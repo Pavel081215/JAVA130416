5b7d3e670a566e9e3f49f43f701ceb76663abc3b
pavel081215 <pavelzag12@gmail.com>
2015-12-17 15:18:49 +0200

    3 модуль

---
+++ Bouquet.java
@@ -0,0 +1,12 @@
+package first;
+
+/**
+ * Created by P.Zagrebelniy on 17.12.2015.
+ */
+public class Bouquet{
+
+    Aster first = new Aster();
+    Rose second = new Rose();
+    Tulip third = new Tulip();
+
+}
