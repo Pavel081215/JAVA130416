91734a56bf3d2a7977895fafc4238cfb671ae1f7
pavel081215 <pavelzag12@gmail.com>
2015-12-20 12:51:50 +0200

    11

---
+++ модуль 3/Flowers.java
@@ -0,0 +1,36 @@
+
+
+/**
+ * Created by P.Zagrebelniy on 17.12.2015.
+ */
+public class Flowers
+{
+    public Flowers (){
+   }
+}
+
+class Rose extends Flowers {
+    public Rose (){
+    }
+
+}
+class Tulip extends Flowers {
+    public Tulip (){
+    }
+
+}
+class Aster extends Flowers {
+    public Aster (){
+    }
+
+}
+class Chamomile extends Flowers {
+    public Chamomile (){
+    }
+
+}
+class Rosebush extends Rose {
+    public Rosebush(){
+    }
+}
+
