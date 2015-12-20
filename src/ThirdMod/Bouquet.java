a40be3e0e86c24bca3cb7a875d1eb4073654b5d7
pavel081215 <pavelzag12@gmail.com>
2015-12-20 13:20:51 +0200

    ThirdMod

---
+++ src/ThirdMod/Bouquet.java
@@ -0,0 +1,15 @@
+package ThirdMod;
+
+public class Bouquet{
+    public static void main(String[] args) {
+
+    Flowers first = new Flowers();
+    Flowers second = new Flowers();
+
+    }
+
+   /* Aster first = new Aster();
+    Rose second = new Rose();
+    Tulip third = new Tulip();*/
+
+}
---
+++ src/ThirdMod/Flowers.java
@@ -0,0 +1,34 @@
+package ThirdMod;
+
+
+public class Flowers
+{
+    public Flowers (){
+   }
+}
+
+/*class Rose extends Flowers {
+    public Rose (){
+    }
+
+}
+class Tulip extends Flowers {
+    public Tulip (){
+    }
+
+}
+/*class Aster extends Flowers {
+    public Aster (){
+    }
+
+}
+class Chamomile extends Flowers {
+    public Chamomile (){
+    }
+
+}
+class Rosebush extends Rose {
+    public Rosebush(){
+    }
+}
+*/
