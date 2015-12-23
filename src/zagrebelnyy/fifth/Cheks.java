d53f57afafd49c35a08f25da14ade0bd462ed4e5
pavel081215 <pavelzag12@gmail.com>
2015-12-23 15:41:14 +0200

    58

--- src/zagrebelnyy/fifth/Cheks.java
+++ src/zagrebelnyy/fifth/Cheks.java
@@ -3,7 +3,7 @@ package zagrebelnyy.fifth;
 /**
  * Created by P.Zagrebelniy on 23.12.2015.
  */
-public class Chek {
+public class Cheks {
     public static void main(String[] args) {
         int[] w = {-1222,3333533,3,4,655555555,7,-8888,5,2,22,44,2222222};
         System.out.println(Sortmaxmin.maxSort(w));
