14f009d93cd3aa1407908814cc676bddfa959dc2
pavel081215 <pavelzag12@gmail.com>
2015-12-23 15:15:53 +0200

    5 модуль

---
+++ src/zagrebelnyy/fifth/Sortmaxmin.java
@@ -0,0 +1,23 @@
+package zagrebelnyy.fifth;
+
+/**
+ * Created by P.Zagrebelniy on 23.12.2015.
+ */
+public class Sortmaxmin {
+    public static int maxSort (int [] a){
+            int max = a[0];
+        for (int q = 0; q < a.length;q ++) {
+            if (max < a[q]){
+            max = a[q];}
+            }
+        return  max; }
+
+    public static int minSort (int [] a){
+        int min = a[0];
+        for (int q = 0; q < a.length;q ++) {
+            if (min > a[q]){
+                min = a[q];}
+        }
+        return  min; }
+    }
+
---
+++ src/zagrebelnyy/fifth/chek.java
@@ -0,0 +1,13 @@
+package zagrebelnyy.fifth;
+
+/**
+ * Created by P.Zagrebelniy on 23.12.2015.
+ */
+public class chek {
+    public static void main(String[] args) {
+        int[] w = {-2222,333333333,3,4,655555555,7,-8888,5,2,22,44,2222222};
+        System.out.println(Sortmaxmin.maxSort(w));
+        System.out.println(Sortmaxmin.minSort(w));
+
+    }
+}
