21d442fd06b204c8dc1d021720fb0a9366a9b84c
pavel081215 <pavelzag12@gmail.com>
2015-12-24 08:33:01 +0200

    yt

--- src/zagrebelnyy/fifth/Sortmaxmin.java
+++ src/zagrebelnyy/fifth/Sortmaxmin.java
@@ -4,20 +4,24 @@ package zagrebelnyy.fifth;
  * Created by P.Zagrebelniy on 23.12.2015.
  */
 public class Sortmaxmin {
-    public static int maxSort (int [] a){
-            int max = a[0];
-        for (int q = 0; q < a.length;q ++) {
-            if (max < a[q]){
-            max = a[q];}
+    public static int maxSort(int[] a) {
+        int max = a[0];
+        for (int q = 0; q < a.length; q++) {
+            if (max < a[q]) {
+                max = a[q];
             }
-        return  max; }
-
-    public static int minSort (int [] a){
-        int min = a[0];
-        for (int q = 0; q < a.length;q ++) {
-            if (min > a[q]){
-                min = a[q];}
         }
-        return  min; }
+        return max;
     }
+
+    public static int minSort(int[] a) {
+        int min = a[0];
+        for (int q = 0; q < a.length; q++) {
+            if (min > a[q]) {
+                min = a[q];
+            }
+        }
+        return min;
+    }
+}
 
