1da226187c5d4dea10d3180ad3dddd0f0a100c7b
pavel081215 <pavelzag12@gmail.com>
2015-12-25 22:18:35 +0200

    6 mod ALL

--- src/zagrebelnyy/six/file/File.java
+++ src/zagrebelnyy/six/file/File.java
@@ -1,11 +1,17 @@
 package zagrebelnyy.six.file;
 
+import java.io.FileNotFoundException;
+
 public abstract class File {
     private String name;
 
-    public File(String name) {
+    public File(String name) throws FileNotFoundException {
 
-        this.name = name;
+        if (name != null) {
+            this.name = name;
+        } else {
+            throw new FileNotFoundException(name);
+        }
     }
 
     public String getName() {
